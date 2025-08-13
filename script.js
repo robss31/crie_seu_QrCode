4f27027a5a3e745288b008e203d102011480762f
Robson Soares <90120510+robss31@users.noreply.github.com>
2025-08-13 00:54:00 -0300

    Update script.js

--- script.js
+++ script.js
@@ -21,7 +21,8 @@ generateBtn.addEventListener('click',() => {
         return;
     }
     generateBtn.innerText = "Gerando um Qr Code...";    
-    qrImg.src =`https://api.qrserver.com/v1/create-qr-code/?size=170x170&data=${qrValue}`;    
+    //qrImg.src =`https://api.qrserver.com/v1/create-qr-code/?size=170x170&data=${qrValue}`; 
+    qrImg.src = https://api.qrcodecreator.com/v1/qrcode/generate`;
     qrImg.addEventListener('load', () => {
         generateBtn.innerText = "Gerar Qr Code";
         container.classList.add('active');
